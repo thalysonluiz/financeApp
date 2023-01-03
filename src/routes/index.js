89988856163b0b10a85cf88f7ffa69efd72b4994
Thalyson Luiz <thalysonluiz@gmail.com>
2023-01-03 15:38:01 -0300

    loading component

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,17 @@ import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
 import { useContext } from "react";
 import { AuthContext } from "../contexts/auth";
+import { Loading } from "../components/Loading";
 
 export function Routes() {
-  const { signed } = useContext(AuthContext)
+  const { signed, loading } = useContext(AuthContext)
+
+  if (loading) {
+    return <Loading />
+  }
 
   return (
     signed ? <AppRoutes /> : <AuthRoutes />
   )
+
 }
